2985a9e3e82b0a7d103d2e048f3bef45a7e6d7cc
ViniciusMoreno123 <93357216+ViniciusMoreno123@users.noreply.github.com>
2021-12-16 20:43:26 -0400

    16/12/2021

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Jogo-tower-Defense
+# PRO-C22-wireframe
+wire frame for c22 
---
+++ sketch.js
@@ -0,0 +1,54 @@
+var engine, world, ground;
+var fundoimg;
+var dftorre, torreimg;
+var torreangulo;
+var canhao;
+
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+
+
+function preload() {
+  fundoimg = loadImage("./assets/background.gif");
+  torreimg = loadImage("./assets/tower.png");
+}
+
+function setup() {
+
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+
+  var options = {
+    isStatic: true
+  }
+  
+  ground = Bodies.rectangle(0, height-1, width-2, 1, options);
+  World.add(world,ground);
+
+  dftorre = Bodies.rectangle(160, 350, 160, 310, options);
+  World.add(world,dftorre);
+ torreangulo = 20;
+ canhao = new Canhao(180,110,130,100,torreangulo);
+}
+
+function draw() {
+  background(189);
+  image(fundoimg, 0, 0, 1200, 600);
+ 
+  Engine.update(engine);
+  
+  rect(ground.position.x, ground.position.y, width*2, 1); 
+  push();
+  imageMode(CENTER);
+  image(torreimg, dftorre.position.x, dftorre.position.y, 160, 310);
+  pop();
+  
+
+
+  canhao.display();
+}
